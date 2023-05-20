1f02d754a5e68fdb3d13f6017bfb3a636f344b73
lauramelotrucco <laura.melo@uc.cl>
2023-05-19 22:06:20 -0400

    cambio Laura (19/5)

--- archivo2.js
+++ archivo2.js
@@ -3,57 +3,34 @@ $(document).ready(function(){
         window.location.href = "archivo1.1.html";
     });
     $(".boton2").click(function(){
-        window.location.href = "archivo1.2.html";
+        window.location.href = "archivo1.4.html";
     });
     $(".boton3").click(function(){
-        window.location.href = "archivo1.3.html";
+        window.location.href = "archivo1.3.1.html";
     });
-    $(".boton4").click(function(){
-        window.location.href = "archivo1.1.html";
-    });
+    $(".boton4").click(function() {
+        var outputValue = $("#output").text();
+      
+        if (outputValue === "60%") {
+          window.location.href = "archivo1.1.1.html";
+        } else {
+          window.location.href = "archivo1.1.2.html";
+        }
+      });      
     $(".boton5").click(function(){
         window.location.href = "archivo1.0.html";
     });  
-    $("#year1").hover(function(){
-        $("#mensaje1").show();
-        },
-        function(){
-        $("#mensaje1").hide();
+    $(".boton6").click(function(){
+        window.location.href ="archivo1.3.1.html";
     });
-    $("#year2").hover(function(){
-        $("#mensaje2").show();
-        },
-        function(){
-        $("#mensaje2").hide();
+    $(".boton7").click(function(){
+        window.location.href = "archivo1.3.2.html";
     });
-    $("#year3").hover(function(){
-        $("#mensaje3").show();
-        },
-        function(){
-        $("#mensaje3").hide();
+    $(".boton8").click(function(){
+        window.location.href = "archivo1.3.3.html";
     });
-    $("#year4").hover(function(){
-        $("#mensaje4").show();
-        },
-        function(){
-        $("#mensaje4").hide();
-    });
-    $("#year5").hover(function(){
-        $("#mensaje5").show();
-        },
-        function(){
-        $("#mensaje5").hide();
-    });
-    $("#year6").hover(function(){
-        $("#mensaje6").show();
-        },
-        function(){
-        $("#mensaje6").hide();
-    });
-    $("#botoninicial3").hover(function(){
-        $("#hazclick").show();
-        },
-        function(){
-        $("#hazclick").hide();
+    $(".boton9").click(function(){
+        window.location.href = "archivo1.2.html";
     });
 });
+
