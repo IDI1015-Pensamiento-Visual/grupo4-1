cc2a9b2d5a91ff026af98fb4046e2f4811937293
lauramelotrucco <laura.melo@uc.cl>
2023-05-21 22:16:40 -0400

    laura cambios (2/21)

--- archivo2.js
+++ archivo2.js
@@ -30,7 +30,127 @@ $(document).ready(function(){
         window.location.href = "archivo1.3.3.html";
     });
     $(".boton9").click(function(){
+        window.location.href = "archivo1.1.3.html";
+    });
+    $(".boton10").click(function(){
+        window.location.href = "archivo1.3.4.html";
+    });
+    $(".boton11").click(function(){
         window.location.href = "archivo1.2.html";
     });
-});
+/*clicks de archivo1.3.4*/
+    $('#f1').click(function() {
+        $("#f3").toggle();
+    });
+    $('#w1').click(function() {
+        $("#w3").toggle();
+    });
+    $('#i1').click(function() {
+        $("#i3").toggle();
+    });
+    $('#s1').click(function() {
+        $("#s3").toggle();
+    });
+    $('#t1').click(function() {
+        $("#t3").toggle();
+    });
+        
+    var mensaje = $('#mensajeayuda');
+    var content = $('#textoayuda').offset().top;
+      
+    $(window).scroll(function() {
+          if ($(window).scrollTop() > content) {
+            mensaje.addClass('hidden'); 
+          } else {
+            mensaje.removeClass('hidden'); 
+          }
+        });
+      
 
+/*hovers de archivo1.4*/
+    $("#addiccion1").hover(function() {
+        $("#addiccion2").show();
+        },
+        function(){
+            $("#soledad2").hide();
+        },
+        );
+        $("#addiccion1").hover(function() {
+            $("#addiccion2").show();
+            },
+            function(){
+                $("#exp2").hide();
+            },
+        );
+        $("#addiccion1").hover(function() {
+                $("#addiccion2").show();
+                },
+                function(){
+                    $("#cyber2").hide();
+                },
+                );
+    $("#soledad1").hover(function() {
+        $("#soledad2").show();
+        },
+        function(){
+            $("#addiccion2").hide();
+            },
+        );
+        $("#soledad1").hover(function() {
+            $("#soledad2").show();
+            },
+            function(){
+                    $("#exp2").hide();
+                },
+            );
+            $("#soledad1").hover(function() {
+                $("#soledad2").show();
+                },
+                function(){
+                        $("#cyber2").hide();
+                    },
+                );
+    $("#exp1").hover(function() {
+        $("#exp2").show();
+            },
+        function(){
+            $("#addiccion2").hide();
+            },
+        );
+        $("#exp1").hover(function() {
+            $("#exp2").show();
+                },
+            function(){
+                $("#soledad2").hide();
+                },
+            );
+            $("#exp1").hover(function() {
+                $("#exp2").show();
+                    },
+                function(){
+                    $("#cyber2").hide();
+                    },
+                );
+    $("#cyber1").hover(function() {
+        $("#cyber2").show();
+            },
+        function(){
+            $("#addiccion2").hide();
+            },
+        );
+        $("#cyber1").hover(function() {
+            $("#cyber2").show();
+                },
+            function(){
+                $("#soledad2").hide();
+                },
+            );
+            $("#cyber1").hover(function() {
+                $("#cyber2").show();
+                    },
+                function(){
+                    $("#exp2").hide();
+                    },
+                );  
+
+});
