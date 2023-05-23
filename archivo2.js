4447374a1beb0db54f16504163ec7b03607961d8
lauramelotrucco <laura.melo@uc.cl>
2023-05-22 22:12:33 -0400

    final

--- archivo2.js
+++ archivo2.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+/*Los siguientes 11 botones te llevan a otro archivo cuando haces click en este*/
     $(".boton1").click(function(){
         window.location.href = "archivo1.2.html";
     });
@@ -9,14 +10,16 @@ $(document).ready(function(){
         window.location.href = "archivo1.3.1.html";
     });
     $(".boton4").click(function() {
-        var outputValue = $("#output").text();
+        var valor = $("#output").text();
       
-        if (outputValue === "60%") {
+        if (valor === "60%") {
           window.location.href = "archivo1.1.1.html";
         } else {
           window.location.href = "archivo1.1.2.html";
         }
-      });      
+      });   
+/*El boton4 te lleva a distintos archivos dependiendo del valor que eligan en el slider los usuarios 
+de la página web. Así si eligen el valor real, les podemos decir que su respuesta es correcta*/   
     $(".boton5").click(function(){
         window.location.href = "archivo1.0.html";
     });  
@@ -38,7 +41,8 @@ $(document).ready(function(){
     $(".boton11").click(function(){
         window.location.href = "archivo1.2.html";
     });
-/*clicks de archivo1.3.4*/
+/*clicks de archivo1.3.4. Cuando haces click en estos botones te muestra un svg (si es que esta hidden)
+y te lo esconde (si es que esta shown). El comando toggle no los mostró Chat GBT*/
     $('#f1').click(function() {
         $("#f3").toggle();
     });
@@ -53,10 +57,11 @@ $(document).ready(function(){
     });
     $('#t1').click(function() {
         $("#t3").toggle();
-    });
-        
+    });   
 
-/*hovers de archivo1.4*/
+/*hovers de archivo1.4 Estos hovers muestran un svg cuando pasasel mouse por sus titulos,
+ y lo esconden cuando pasas el mouse por otro de los titulo (no cuando haces mouseleave, 
+así la información esta hasta quieras ver la información de otro)*/
     $("#addiccion1").hover(function() {
         $("#addiccion2").show();
         },
@@ -141,6 +146,8 @@ $(document).ready(function(){
                     $("#exp2").hide();
                     },
                 );  
+/*Este código permite que el mensaje de ayuda desaparezca cuando hacen scroll y
+ pasan el textoayuda. CHat GBT nos ayudo con este código*/
     var mensaje = $('#mensajeayuda');
     var content = $('#textoayuda').offset().top;
       
